3e64856ee5e1c11e1dcbbffc591010136af8f823
Den <denis-s2@yandex.ru>
2023-05-22 18:12:23 +0500

    связал календарь и dateTime

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QDialog
-from ugk import Ui_ugk_planner
+from qtpy import uic
 
-app = QApplication(sys.argv)
-window = QDialog()
-ui = Ui_ugk_planner()
-ui.setupUi(window)
+Form, Window = uic.loadUiType("ugk_planner.ui")
+
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+
+
+def one_click():
+    print(form.calendarWidget.selectedDate().toString('dd-MM-yyyy'))
+
+
+def one_click_calendar():
+    print(form.calendarWidget.selectedDate().toString('dd-MM-yyyy'))
+    form.dateTimeEdit.setDate(form.calendarWidget.selectedDate())
+
+
+def one_dateedit_change():
+    print(form.dateTimeEdit.dateTime().toString('dd-MM-yyyy'))
+    form.calendarWidget.setSelectedDate(form.dateTimeEdit.date())
+
+
+form.pushButton.clicked.connect(one_click)
+form.calendarWidget.clicked.connect(one_click_calendar)
+form.dateTimeEdit.dateChanged.connect(one_dateedit_change)
 
 window.show()
+
 sys.exit(app.exec())
--- ugk_planner.py
+++ ugk_planner.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file 'ui_imagedialog.ui'
+# Form implementation generated from reading ui file 'ugk_planner.ui'
 #
 # Created by: PyQt6 UI code generator 6.5.0
 #
@@ -12,13 +12,9 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 class Ui_ugk_planner(object):
     def setupUi(self, ugk_planner):
         ugk_planner.setObjectName("ugk_planner")
-        ugk_planner.resize(986, 709)
+        ugk_planner.resize(998, 709)
         ugk_planner.setAutoFillBackground(False)
-        self.buttonBox = QtWidgets.QDialogButtonBox(parent=ugk_planner)
-        self.buttonBox.setGeometry(QtCore.QRect(790, 650, 171, 32))
-        self.buttonBox.setOrientation(QtCore.Qt.Orientation.Horizontal)
-        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.StandardButton.Cancel|QtWidgets.QDialogButtonBox.StandardButton.Ok)
-        self.buttonBox.setObjectName("buttonBox")
+        ugk_planner.setStyleSheet("")
         self.calendarWidget = QtWidgets.QCalendarWidget(parent=ugk_planner)
         self.calendarWidget.setGeometry(QtCore.QRect(40, 330, 271, 301))
         self.calendarWidget.setObjectName("calendarWidget")
@@ -33,25 +29,27 @@ class Ui_ugk_planner(object):
         self.textEdit_2.setObjectName("textEdit_2")
         self.dateTimeEdit = QtWidgets.QDateTimeEdit(parent=ugk_planner)
         self.dateTimeEdit.setGeometry(QtCore.QRect(40, 650, 271, 24))
+        self.dateTimeEdit.setButtonSymbols(QtWidgets.QAbstractSpinBox.ButtonSymbols.UpDownArrows)
         self.dateTimeEdit.setObjectName("dateTimeEdit")
         self.textEdit_3 = QtWidgets.QTextEdit(parent=ugk_planner)
         self.textEdit_3.setGeometry(QtCore.QRect(40, 100, 271, 51))
         self.textEdit_3.setObjectName("textEdit_3")
-        self.textEdit_4 = QtWidgets.QTextEdit(parent=ugk_planner)
-        self.textEdit_4.setGeometry(QtCore.QRect(340, 20, 621, 611))
-        self.textEdit_4.setObjectName("textEdit_4")
         self.pushButton = QtWidgets.QPushButton(parent=ugk_planner)
         self.pushButton.setGeometry(QtCore.QRect(340, 650, 113, 32))
+        self.pushButton.setStyleSheet("")
         self.pushButton.setObjectName("pushButton")
+        self.tableWidget = QtWidgets.QTableWidget(parent=ugk_planner)
+        self.tableWidget.setGeometry(QtCore.QRect(340, 20, 621, 611))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
 
         self.retranslateUi(ugk_planner)
-        self.buttonBox.accepted.connect(ugk_planner.accept) # type: ignore
-        self.buttonBox.rejected.connect(ugk_planner.reject) # type: ignore
         QtCore.QMetaObject.connectSlotsByName(ugk_planner)
 
     def retranslateUi(self, ugk_planner):
         _translate = QtCore.QCoreApplication.translate
-        ugk_planner.setWindowTitle(_translate("ugk_planner", "UGK"))
+        ugk_planner.setWindowTitle(_translate("ugk_planner", "UGK_planneer"))
         self.textEdit.setHtml(_translate("ugk_planner", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
@@ -68,10 +66,5 @@ class Ui_ugk_planner(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:36pt;\">Кафедра</span></p></body></html>"))
-        self.textEdit_4.setHtml(_translate("ugk_planner", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'.AppleSystemUIFont\'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:36pt;\">Таблица с расписанием</span></p></body></html>"))
-        self.pushButton.setText(_translate("ugk_planner", "PushButton"))
+        self.pushButton.setWhatsThis(_translate("ugk_planner", "<html><head/><body><p><br/></p></body></html>"))
+        self.pushButton.setText(_translate("ugk_planner", "Сохранить"))
